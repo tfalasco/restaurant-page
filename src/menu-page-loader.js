a2a34a5d8461ca4a58657813dbab8b2543618e23
Ted Falasco <14131511+tfalasco@users.noreply.github.com>
2025-02-28 05:08:19 -0500

    Add a module to load the contents of the menu page

---
+++ src/menu-page-loader.js
@@ -0,0 +1,49 @@
+import "./menu-styles.css";
+
+export { loadMenuPage };
+
+function loadMenuPage() {
+        // Create a div to hold the contents of this tab
+        const contentDiv = document.createElement("div");
+        
+        // Create the header and append it to the contentDiv
+        const header = document.createElement("h1");
+        header.innerText = "Ted's Tater Tots";
+        contentDiv.appendChild(header);
+        
+        // Create the sub-header and append it to the contentDiv
+        const subHeader = document.createElement("h2");
+        subHeader.innerText = "Menu";
+        contentDiv.appendChild(subHeader);
+        
+        // Create menu items and append them to the contentDiv
+        contentDiv.appendChild(createMenuItem("Tots", "Your standard tot.", "$6 / doz"));
+        contentDiv.appendChild(createMenuItem("Deluxe Tots", "Tots with dipping sauce.", "$10 / doz"));
+    
+        // return the contentDiv
+        return contentDiv;
+}
+
+function createMenuItem(name, description, price) {
+    // Create a div to hold the contents of this menu item
+    const menuItem = document.createElement("div");
+    menuItem.setAttribute("class", "menuItem");
+
+    // Add the item name to the menuItem div
+    const itemName = document.createElement("h3");
+    itemName.innerText = name;
+    menuItem.appendChild(itemName);
+
+    // Add the description to the menuItem div
+    const itemDescription = document.createElement("p");
+    itemDescription.innerText = description;
+    menuItem.appendChild(itemDescription);
+
+    // Add the price
+    const itemPrice = document.createElement("p");
+    itemPrice.style.fontWeight = 'bold';
+    itemPrice.innerText = price;
+    menuItem.appendChild(itemPrice);
+
+    return menuItem;
+}
